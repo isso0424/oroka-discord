08cfcf8051322cd4edd903582437d4f064022d2c
isso0424 <isso04240129@gmail.com>
2021-06-04 12:10:30 +0900

    feat: add constructor for discord

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	discord, err := discordgo.New()
+	discord.Token = ""
+	if err != nil {
+		panic(err)
+	}
+
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	defer discord.Close()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	<-sc
 }
